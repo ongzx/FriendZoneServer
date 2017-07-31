62ba8b0b434f6624138bb643f3bbd153eff64736
chihi <chihi@aporigin.com>
2017-07-31 20:30:29 +0800

    change port number

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@ export default {
     // Mariadb
     db_name_mariadb: 'friendzonedb',
     db_dialect_mariadb: 'mysql',
-    db_host_mariadb: 'mariadb_friendzone',
+    db_host_mariadb: '52.221.227.175',
     db_port_mariadb: '3306',
     seeder_storage_mariadb: 'sequelize',
     port: 3000
--- dist/config/env/development.js
+++ dist/config/env/development.js
@@ -8,7 +8,7 @@ exports.default = {
     // Mariadb
     db_name_mariadb: 'friendzonedb',
     db_dialect_mariadb: 'mysql',
-    db_host_mariadb: 'mariadb_friendzone',
+    db_host_mariadb: '52.221.227.175',
     db_port_mariadb: '3306',
     seeder_storage_mariadb: 'sequelize',
     port: 3000
