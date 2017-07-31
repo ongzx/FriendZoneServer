5fb24aacb8e007fad00777d36892f99fbfffc4c1
chihi <chihi@aporigin.com>
2017-08-01 00:24:32 +0800

    change dev setting

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ export default {
     db_name_mariadb: 'friendzonedb',
     db_dialect_mariadb: 'mysql',
     db_host_mariadb: '52.221.227.175',
-    db_port_mariadb: '3306',
+    db_port_mariadb: '32770',
     seeder_storage_mariadb: 'sequelize',
     port: 3000
 }
---
+++ init_db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE friendzonedb;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' identified by 'password';
+FLUSH PRIVILEGES;
